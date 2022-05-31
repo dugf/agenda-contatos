84d5ec94c5e631233ea38cbb8162f72d78afb553
dugf <c.eduardo.g.f.santos@gmail.com>
2022-05-30 21:34:09 -0300

    Projeto Completo

---
+++ app_contacts/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:app_contacts/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
